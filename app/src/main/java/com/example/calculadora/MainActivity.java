53e22b909c5f27f51e2be0154e167978f0668d5c
willian_warmling <willian_warmling@estudante.sesisenai.org.br>
2023-03-27 08:48:01 -0300

    atualização

--- app/src/main/java/com/example/calculadora/MainActivity.java
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -3,12 +3,50 @@ package com.example.calculadora;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
+    private EditText txtet;
+    private EditText txtet2;
+    private TextView resultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txtet = findViewById(R.id.et);
+        txtet2 = findViewById(R.id.et2);
+        resultado = findViewById(R.id.resultado);
+    }
+
+    public void somar(View view) {
+        int valor1 = Integer.parseInt(txtet.getText().toString());
+        int valor2 = Integer.parseInt(txtet2.getText().toString());
+
+        resultado.setText(String.valueOf(valor1 + valor2));
+    }
+
+    public void subtrair(View view) {
+        int valor1 = Integer.parseInt(txtet.getText().toString());
+        int valor2 = Integer.parseInt(txtet2.getText().toString());
+
+        resultado.setText(String.valueOf(valor1 + valor2));
+    }
+
+    public void multiplicar(View view) {
+        int valor1 = Integer.parseInt(txtet.getText().toString());
+        int valor2 = Integer.parseInt(txtet2.getText().toString());
+
+        resultado.setText(String.valueOf(valor1 * valor2));
+    }
+
+    public void dividir(View view) {
+        int valor1 = Integer.parseInt(txtet.getText().toString());
+        int valor2 = Integer.parseInt(txtet2.getText().toString());
+
+        resultado.setText(String.valueOf(valor1 / valor2));
     }
 }
